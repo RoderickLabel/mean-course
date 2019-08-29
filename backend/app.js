99c7e17e5f6277cc6fb8cd4d45fbad6970e252a0
RoderickLabel <roderickruotolo@gmail.com>
2019-08-29 15:41:43 -0300

    40 Class - Adding the POST Backend Point

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:false }));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -14,7 +18,13 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/api/posts', (req, res, next) => {
+app.post("/api/posts", (req, res, next) => {
+    const posts = req.body;
+    console.log(post);
+    res.status(201).json();
+});
+
+app.use("/api/posts", (req, res, next) => {
     const posts = [
         {
             id: 'fadf12421l',
