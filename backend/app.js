578dc5128e5971a91c720acf23cbf364763e7060
RoderickLabel <roderickruotolo@gmail.com>
2019-08-29 17:19:11 -0300

    51 Class - Creating a POST instance

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
+const Post = require("./models/post");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:false }));
@@ -19,8 +20,11 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-    const posts = req.body;
-    console.log(posts);
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    });
+    console.log(post);
     res.status(201).json({
         message: "Post added successfully"
     });
